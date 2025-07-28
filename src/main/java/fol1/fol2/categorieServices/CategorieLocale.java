f646c5a5880ef7829a804743bd52a5c9eab57e9c
abdellah <abdellahazaz5@gmail.com>
2025-07-28 19:29:57 +0200

    commentaire

--- src/main/java/fol1/fol2/categorieServices/CategorieLocale.java
+++ src/main/java/fol1/fol2/categorieServices/CategorieLocale.java
@@ -6,6 +6,8 @@ import javax.ejb.Local;
 
 import fol1.fol2.entities.Categorie;
 
+//ajout d un comments  
+
 @Local
 public interface CategorieLocale {
     Categorie addCategorie(Categorie categorie);
